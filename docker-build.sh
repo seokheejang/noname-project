f29282eebaf1c9224fa54a1c9155b6d7bd57b34e
seokheejang <shjang@dkargo.io>
2025-01-20 18:54:15 +0900

    Add registry
    denied 이슈 발생

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,13 @@
 docker build -t msa-server:latest ./msa-server
 docker build -t gateway:latest ./gateway
 docker build -t client:latest ./client
+
+docker tag gateway:latest local-registry:5000/gateway:latest
+docker tag msa-server:latest local-registry:5000/msa-server:latest
+docker tag client:latest local-registry:5000/client:latest
+
+# 로컬에 올릴 때 HTTPS Proxy 문제 발생하면, 
+# sudo vi /etc/hosts 열어서 "127.0.0.1 local-registry" 추가
+docker push local-registry:5000/gateway:latest
+docker push local-registry:5000/msa-server:latest
+docker push local-registry:5000/client:latest
--- kube-apply.sh
+++ kube-apply.sh
@@ -1,2 +1,3 @@
+kubectl apply -f k8s/registry-deployment.yaml
 kubectl apply -f k8s/deployment.yaml
 kubectl apply -f k8s/service.yaml
